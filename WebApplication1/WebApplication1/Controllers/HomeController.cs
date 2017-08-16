e4c8df1c801058c6eb596b3f184c6b8075c5304a
SomeGuy <someguy@virtual-affairs.com>
2017-08-16 14:39:45 +0200

    Change HomeController

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace WebApplication1.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController2 : Controller
     {
         public ActionResult Index()
         {
